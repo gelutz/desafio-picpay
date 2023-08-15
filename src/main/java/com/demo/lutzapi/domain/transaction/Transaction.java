888391673e52af63653341f0a561be010a8f4b2c
gelutz <gabriel.lutz5@gmail.com>
2023-08-15 14:27:50 -0300

    added: Transaction entity

---
+++ src/main/java/com/demo/lutzapi/domain/transaction/Transaction.java
@@ -0,0 +1,26 @@
+package com.demo.lutzapi.domain.transaction;
+
+import com.demo.lutzapi.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="buyerId")
+    private User buyer;
+
+    @ManyToOne
+    @JoinColumn(name="sellerId")
+    private User seller;
+}
