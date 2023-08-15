943b1b135f2c2bb69e0075906dfc09b86cab7f7c
gelutz <gabriel.lutz5@gmail.com>
2023-08-15 14:16:22 -0300

    Adding initial files

---
+++ src/main/java/com/demo/lutzapi/LutzapiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.lutzapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LutzapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LutzapiApplication.class, args);
+	}
+
+}
