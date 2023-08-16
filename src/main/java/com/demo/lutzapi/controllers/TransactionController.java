b91c36898e4b75a87cfab6903e64ca6f6c714b96
gelutz <gabriel.lutz5@gmail.com>
2023-08-16 13:35:12 -0300

    feat: criando TransactionController

---
+++ src/main/java/com/demo/lutzapi/controllers/TransactionController.java
@@ -0,0 +1,11 @@
+package com.demo.lutzapi.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+@AllArgsConstructor
+public class TransactionController {
+}
