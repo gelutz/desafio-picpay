efa72000d31a39c4402d6e5819ed37ffa2b586ad
gelutz <gabriel.lutz5@gmail.com>
2023-09-06 17:36:00 -0300

    feat: iniciando os testes dos controllers

--- src/test/java/com/lutzapi/presentation/controllers/UserControllerTest.java
+++ src/test/java/com/lutzapi/presentation/controllers/UserControllerTest.java
@@ -1,13 +1,35 @@
 package com.lutzapi.presentation.controllers;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {}
+
     @Test
-    void defaultTest() {
-        assert true;
+    @DisplayName("The endpoint should return an empty list when there are no users")
+    public void itShouldReturnAnEmptyArray() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
     }
 }
