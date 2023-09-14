508d2e8979b671bd34dbeed0a27d2a3afe1f03cc
gelutz <gabriel.lutz5@gmail.com>
2023-09-14 14:27:52 -0300

    feat: alterando JpaRepository para CrudRepository

--- src/main/java/com/lutzapi/infrastructure/repositories/TransactionRepository.java
+++ src/main/java/com/lutzapi/infrastructure/repositories/TransactionRepository.java
@@ -1,7 +1,7 @@
 package com.lutzapi.infrastructure.repositories;
 
 import com.lutzapi.domain.entities.transaction.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends CrudRepository<Transaction, Long> {
 }
