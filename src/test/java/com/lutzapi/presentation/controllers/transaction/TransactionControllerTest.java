502875e72968c44ccbbb6d8c77cd261514651b1f
Gabriel Lutz <gabriell@rsdata.inf.br>
2023-09-14 14:26:06 -0300

    feat: movendo TransactionControllerTest para a pasta correta

--- src/test/java/com/lutzapi/presentation/controllers/transaction/TransactionControllerTest.java
+++ src/test/java/com/lutzapi/presentation/controllers/transaction/TransactionControllerTest.java
@@ -1,7 +1,6 @@
-package com.lutzapi.presentation.controllers;
+package com.lutzapi.presentation.controllers.transaction;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
