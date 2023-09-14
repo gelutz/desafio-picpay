2b67308fe2fa39bca2ed5057e91030ac7e1f9f88
Gabriel Lutz <gabriell@rsdata.inf.br>
2023-09-14 14:28:23 -0300

    feat: adicionando hashCode e equals no MissingDataException

--- src/main/java/com/lutzapi/domain/exceptions/user/MissingDataException.java
+++ src/main/java/com/lutzapi/domain/exceptions/user/MissingDataException.java
@@ -1,9 +1,25 @@
 package com.lutzapi.domain.exceptions.user;
 
 import java.util.List;
+import java.util.Objects;
 
 public class MissingDataException extends RuntimeException {
+    final List<String> requiredFields;
     public MissingDataException(List<String> fields) {
         super("Os seguintes campos estão faltando: " + String.join(", ", fields));
+        requiredFields = fields;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MissingDataException that = (MissingDataException) o;
+        return Objects.equals(requiredFields, that.requiredFields);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requiredFields);
     }
 }
