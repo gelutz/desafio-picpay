7d78cd029018d6f9486630b993bf085ab3f336a9
gelutz <gabriel.lutz5@gmail.com>
2023-09-16 15:24:14 -0300

    fix: ajustando teste do TransactionControllerTest

--- src/test/java/com/lutzapi/presentation/controllers/transaction/TransactionControllerTest.java
+++ src/test/java/com/lutzapi/presentation/controllers/transaction/TransactionControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lutzapi.application.dtos.TransactionDTO;
 import com.lutzapi.application.services.TransactionService;
 import com.lutzapi.domain.entities.transaction.Transaction;
+import com.lutzapi.domain.entities.user.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -43,6 +44,7 @@ class TransactionControllerTest {
         TransactionDTO transactionDTO = mock(TransactionDTO.class);
         Transaction transactionMock = mock(Transaction.class);
 
+        when(transactionMock.getBuyer()).thenReturn(mock(User.class));
         when(transactionMock.getBuyer().getId()).thenReturn(1L);
         when(transactionService.createTransaction(transactionDTO)).thenReturn(transactionMock);
 
