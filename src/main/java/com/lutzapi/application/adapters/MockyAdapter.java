6d3d7e0c65fd02c6b2a292f4e0b9a732cbc16ce7
gelutz <gabriel.lutz5@gmail.com>
2023-09-16 15:23:26 -0300

    feat: movendo URL do mocky para application.properties

--- src/main/java/com/lutzapi/application/adapters/MockyAdapter.java
+++ src/main/java/com/lutzapi/application/adapters/MockyAdapter.java
@@ -4,8 +4,9 @@ import com.lutzapi.application.dtos.MockyTransactionDTO;
 import com.lutzapi.application.interfaces.ApiAdapter;
 import com.lutzapi.domain.exceptions.mocky.MockyAuthException;
 import com.lutzapi.domain.exceptions.mocky.MockyDefaultExceptin;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -13,17 +14,19 @@ import org.springframework.web.client.RestTemplate;
 
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Component
 public class MockyAdapter implements ApiAdapter {
-    final String url = "https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6";
-    private RestTemplate template;
+    private final RestTemplate template;
+
+    @Value("${mocky.url}")
+    private String url;
 
     @Override
     @SuppressWarnings("unchecked")
     public MockyTransactionDTO call() {
         ResponseEntity<MockyTransactionDTO> response = template.getForEntity(url, MockyTransactionDTO.class);
-        if (response.getStatusCode() != HttpStatus.OK || response.getBody() == null) {
+        if (response == null || response.getStatusCode() != HttpStatus.OK || response.getBody() == null) {
             throw new MockyDefaultExceptin("Erro na transação");
         }
 
