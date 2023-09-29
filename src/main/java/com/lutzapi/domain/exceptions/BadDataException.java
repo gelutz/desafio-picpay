009b953e2b06771ffa47890f7d5409f7d0fe8fa4
gelutz <gabriel.lutz5@gmail.com>
2023-09-29 10:50:10 -0300

    feat: adding test to subtractBalance

--- src/main/java/com/lutzapi/domain/exceptions/BadDataException.java
+++ src/main/java/com/lutzapi/domain/exceptions/BadDataException.java
@@ -1,8 +1,10 @@
 package com.lutzapi.domain.exceptions;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class BadDataException extends RuntimeException {
     String message;
 }
