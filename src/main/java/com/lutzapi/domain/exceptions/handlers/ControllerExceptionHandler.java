722214fd80f1c484309bae8f090e15fbe2d1794d
Gabriel Lutz <gabriell@rsdata.inf.br>
2023-10-06 13:54:05 -0300

    fix: removendo validation para recomeçar

--- src/main/java/com/lutzapi/domain/exceptions/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/lutzapi/domain/exceptions/handlers/ControllerExceptionHandler.java
@@ -8,8 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,8 +15,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
@@ -67,19 +63,19 @@ public class ControllerExceptionHandler {
         return new LutzExceptionResponse("API do Mocky retornou 404", null);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    // TODO alterar esse retorno para um LutzExceptionResponse
-    public Map<String, String> handle(MethodArgumentNotValidException exception) {
-        externalLog(exception);
-
-        Map<String, String> errors = new HashMap<>();
-        exception.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-
-        return errors;
-    }
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    // TODO alterar esse retorno para um LutzExceptionResponse
+//    public Map<String, String> handle(MethodArgumentNotValidException exception) {
+//        externalLog(exception);
+//
+//        Map<String, String> errors = new HashMap<>();
+//        exception.getBindingResult().getAllErrors().forEach((error) -> {
+//            String fieldName = ((FieldError) error).getField();
+//            String errorMessage = error.getDefaultMessage();
+//            errors.put(fieldName, errorMessage);
+//        });
+//
+//        return errors;
+//    }
 }
