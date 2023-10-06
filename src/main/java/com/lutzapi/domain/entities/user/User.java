08278f98372f91240b17a232356b6ce1545a69c2
gelutz <gabriel.lutz5@gmail.com>
2023-10-06 13:54:05 -0300

    fix: removendo validation para recomeçar

--- src/main/java/com/lutzapi/domain/entities/user/User.java
+++ src/main/java/com/lutzapi/domain/entities/user/User.java
@@ -2,8 +2,6 @@ package com.lutzapi.domain.entities.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -23,29 +21,19 @@ public class User {
     private UUID id;
 
     @Enumerated(EnumType.STRING)
-    @NotNull
-    @NotBlank(message = "User needs to be of type SELLER or BUYER")
     private UserType type;
 
-    @NotNull
-    @NotBlank(message = "First name is mandatory")
     private String firstName;
 
     private String lastName;
 
     @Column(unique = true)
-    @NotNull
-    @NotBlank(message = "Document is mandatory")
     private String document;
 
     @Column(unique = true)
-    @NotNull
-    @NotBlank(message = "Email is mandatory")
     private String email;
 
     @JsonIgnore
-    @NotNull
-    @NotBlank(message = "Password is mandatory")
     private String password;
 
     @JsonIgnore
