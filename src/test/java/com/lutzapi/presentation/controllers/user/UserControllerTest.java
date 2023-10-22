d986b508b1fb08f3ee325cf16dcdcb7cfc64107b
gelutz <gabriel.lutz5@gmail.com>
2023-10-22 11:19:37 -0300

    fix: both controller's tests are working

--- src/test/java/com/lutzapi/presentation/controllers/user/UserControllerTest.java
+++ src/test/java/com/lutzapi/presentation/controllers/user/UserControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Import(ControllerExceptionHandler.class)
+@WithMockUser
 public class UserControllerTest {
     @MockBean
     private UserService userServiceMock;
