868f5e4675b73d6d5031081e38b01efd4d6cd93e
gelutz <gabriel.lutz5@gmail.com>
2023-10-23 17:16:27 -0300

    feat: adicionando flag no .properties para escolher se quer seed ou não

--- src/main/java/com/lutzapi/application/listeners/SeedDatabaseListener.java
+++ src/main/java/com/lutzapi/application/listeners/SeedDatabaseListener.java
@@ -3,28 +3,38 @@ package com.lutzapi.application.listeners;
 import com.lutzapi.application.dtos.CreateTransactionDTO;
 import com.lutzapi.application.services.TransactionService;
 import com.lutzapi.application.services.UserService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.Profiles;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Component
+@RequiredArgsConstructor
 public class SeedDatabaseListener {
-    UserService userService;
-    TransactionService transactionService;
-    Environment environment;
+    final UserService userService;
+    final TransactionService transactionService;
+
+    @Value("${lutzapi.seed}")
+    private String seed;
+
+    final Logger logger = LoggerFactory.getLogger(getClass());
 
     @EventListener
-    public void seed(ContextRefreshedEvent event) {
-        if (environment.acceptsProfiles(Profiles.of("default"))) {
-            List<CreateTransactionDTO> dtos = userService.seed(10);
-            System.out.println(dtos.toString());
-            dtos.forEach(transactionService::saveTransaction);
+    public void seed(@SuppressWarnings("unused") ContextRefreshedEvent event) {
+        if (Boolean.parseBoolean(seed)) {
+            List<CreateTransactionDTO> dtos = userService.seed(100);
+            dtos.forEach((dto) -> {
+                try {
+                    transactionService.saveTransaction(dto);
+                } catch (Exception e) {
+                    logger.info("Houve um erro ao criar a transação: " + e.getMessage());
+                }
+            });
         }
     }
 }
